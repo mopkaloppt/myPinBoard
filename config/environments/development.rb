f0bd9f76c09f019c3b2915151ce3938d21eeea40
Maprang Suwanbubpa <moppi.sgotmoney@gmail.com>
2015-05-05 21:24:28 -0700

    Add jquery masonry and styles (pics not yet centered)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,8 @@ Rails.application.configure do
 
   # Needed for Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Needed for Paperclip
+  Paperclip.options[:command_path] = "/usr/local/bin/"
+
 end
